480ee127c251192867566d61a5a889e5891040d4
Vytis Valentinavičius <vytis.valentinavicius@lamoda.ru>
2014-01-07 21:52:06 +0200

    Fixed zsh autocomplete path.

--- zsh/complete.zsh
+++ zsh/complete.zsh
@@ -1,5 +1,5 @@
 # folder of all of your autocomplete functions
-fpath=($HOME/.yadr/zsh/custom_completions $fpath)
+fpath=($HOME/.yadr/zsh/custom_completions $fpath /usr/local/share/zsh/site-functions)
 
 # enable autocomplete function
 autoload -U compinit
