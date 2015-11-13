7a6c03b9b981cb279f81020b531f05c28bbd5080
Vytis Valentinavičius <vytis.valentinavicius@lamoda.ru>
2015-11-13 19:02:35 +0200

    Returned substring search

--- zsh/key-bindings.zsh
+++ zsh/key-bindings.zsh
@@ -5,6 +5,9 @@
 bindkey -v                                          # Use vi key bindings
 bindkey '^r' history-incremental-search-backward    # [Ctrl-r] - Search backward incrementally for a specified string. The string may begin with ^ to anchor the search to the beginning of the line.
 
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+
 # emacs style
 bindkey '^a' beginning-of-line
 bindkey '^e' end-of-line
