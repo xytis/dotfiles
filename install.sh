15b11036a87799347caae88dcf36733c159cf18b
Vytis Valentinavičius <vytis.valentinavicius@lamoda.ru>
2017-07-12 13:56:09 +0300

    Fixed instalation for future branch

--- install.sh
+++ install.sh
@@ -2,8 +2,9 @@
 
 if [ ! -d "$HOME/.yadr" ]; then
     echo "Installing YADR for the first time"
-    git clone --depth=1 https://github.com/skwp/dotfiles.git "$HOME/.yadr"
+    git clone --depth=1 https://github.com/future/dotfiles.git "$HOME/.yadr"
     cd "$HOME/.yadr"
+    git checkout future
     [ "$1" = "ask" ] && export ASK="true"
     rake install
 else
