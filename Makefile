7046c7ccf609994f621ca1fd8b5348e437e0ca0b
Vytis Valentinavičius <vytis.valentinavicius@lamoda.ru>
2017-10-01 13:12:01 +0300

    Updated install script

--- Makefile
+++ Makefile
@@ -1,9 +1,16 @@
 LN=ln -sv
+DIR=mkdir -p
 
 all: ../.*
 
-../.aprc:
-	$(LN) irb/aprc $@
+../.config:
+	$(DIR) $@
+
+../.config/git: ../.config
+	$(LN) git $@
+
+../.config/nvim: ../.config
+	$(LN) nvim $@
 
 ../.ctags:
 	$(LN) ctags/ctags $@
@@ -11,26 +18,14 @@ all: ../.*
 ../.editrc:
 	$(LN) vimify/editrc $@
 
-../.excaped_colors.rb:
-	$(LN) irb/escaped_colors.rb $@
-
 ../.gemrc:
 	$(LN) ruby/gemrc $@
 
 ../.inputrc:
 	$(LN) vimify/inputrc $@
 
-../.pryrc:
-	$(LN) irb/pryrc $@
-
-../.rdebugrc:
-	$(LN) ruby/rdebugrc $@
-
 ../.tmux.conf:
 	$(LN) tmux/tmux.conf $@
-
-../.unexcaped_colors.rb:
-	$(LN) irb/unescaped_colors.rb $@
 
 ../.zlogin:
 	$(LN) zsh/prezto/runcoms/zlogin $@
--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
+REPO=https://github.com/xytis/dotfiles.git
+
 if [ ! -d "$HOME/.yadr" ]; then
-    echo "Installing YADR for the first time"
-    git clone --depth=1 https://github.com/future/dotfiles.git "$HOME/.yadr"
-    cd "$HOME/.yadr"
-    git checkout future
-    [ "$1" = "ask" ] && export ASK="true"
-    rake install
+  echo "Installing YADR for the first time"
+  git clone --depth=1 \
+    --origin upstream \
+    --branch future \
+    $REPO "$HOME/.yadr"
+  cd "$HOME/.yadr" && make all
 else
-    echo "YADR is already installed"
+  echo "YADR is already installed"
+  echo "=> checking if git remote matches the source"
+  if [ $(git remote get-url upstream) != $REPO ]; then
+    echo "=> changing remote address from $(git remote get-url upstream) to $REPO"
+    git remote set-url upstream $(REPO)
+  fi
 fi
